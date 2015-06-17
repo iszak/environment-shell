1c191d1491f1b8f90da9102735e808e34af4240a
Iszak <iszak.bryan@gmail.com>
2015-06-17 23:49:19 +0100

    Removed redundant code
    Updated bundled, added --yes flags, removed msgpack, corrected paths

--- userdata.sh
+++ userdata.sh
@@ -14,16 +14,17 @@ sudo apt-get install git --yes
 # Install Puppetlabs Repository
 wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
 sudo dpkg -i puppetlabs-release-trusty.deb
+rm puppetlabs-release-trusty.deb
 
 # Install Ruby Repository
-sudo apt-add-repository ppa:brightbox/ruby-ng
+sudo apt-add-repository ppa:brightbox/ruby-ng --yes
 
 
 # Update packages
 sudo apt-get update
 
 # Install puppet
-sudo apt-get install puppet hiera facter
+sudo apt-get install puppet --yes
 
 # Install Ruby 2.1
 sudo apt-get install ruby2.1 ruby2.1-dev --yes
@@ -33,11 +34,10 @@ update-alternatives --set ruby /usr/bin/ruby2.1
 update-alternatives --set gem /usr/bin/gem2.1
 
 # Install bundler
-sudo gem install bundler --version=1.9.9
+sudo gem install bundler --version=1.10.4
 
 # Hiera
 sudo gem install deep_merge --version=1.0.1
-sudo gem install msgpack --version=0.5.12
 
 # Prepare
 sudo rm -rf /etc/puppet/
@@ -45,19 +45,13 @@ sudo rm -rf /var/lib/hiera
 sudo rm -f /etc/hiera.yaml
 
 # Deploy
-git clone --depth=1 https://github.com/iszak/environment-puppet.git /etc/puppet/environments/production/
-
-# Symlink Hiera
-ln -s /etc/puppet/hiera.yaml /etc/hiera.yaml
+git clone --depth=1 https://github.com/iszak/environment-puppet.git /etc/puppet/
 
 # Modules
-cd /etc/puppet/environments/production/
+cd /etc/puppet/
 
 bundle install --deployment
 bundle exec librarian-puppet install
-
-# Clean
-rm puppetlabs-release-trusty.deb
 
 # Configure locales
 dpkg-reconfigure locales
