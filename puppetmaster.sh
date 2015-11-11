6397b01a14b6421997972c97661db6c022369ef9
Iszak <iszak.bryan@gmail.com>
2015-11-11 22:46:47 +0000

    Update puppetmaster.sh

--- puppetmaster.sh
+++ puppetmaster.sh
@@ -1,31 +1,47 @@
 #!/usr/bin/env sh
 set -e
 
-./puppet.sh
-./hiera.sh
-./facter.sh
-
-sudo apt-get install puppetmaster
-
 REPO_URL="https://github.com/iszak/puppet.git"
-REPO_PATH="/etc/puppet"
 
-# Prepare
-rm -rf "$REPO_PATH"
+FACTER_PATH="/etc/facter"
+PUPPET_PATH="/etc/puppet"
+HIERA_PATH="/etc/hiera.yaml"
 
-# Git
+# TODO: Check ruby 2.0+ installed
+# TODO: Check bundle installed
+
+# Librarian
+sudo gem install librarian-puppet
+sudo gem install bundler
+
+# Puppet
+# TODO: Check for ruby-shadow and ruby-deep-merge
+# sudo gem install ruby-shadow
+# sudo gem install deep_merge
+
 sudo apt-get install git --yes
 
-# Bundler
-sudo apt-get install bundler
+# Prepare
+rm -rf "$PUPPET_PATH"
+rm -rf "$FACTER_PATH"
+rm -rf "$HIERA_PATH"
 
-## Deploy
-git clone --depth=1 "$REPO_URL" "$REPO_PATH"
+mkdir -p "$FACTER_PATH/facts.d"
 
-## Modules
+# Puppet + Hiera code
+git clone --depth=1 --recursive "$REPO_URL" "$PUPPET_PATH"
+
+# Puppet Modules
 cd /etc/puppet/
 
 bundle install --deployment
-
-## Ensure latest versions
 bundle exec librarian-puppet update
+
+# Puppet master
+sudo apt-get install puppetmaster --yes
+
+# Role
+echo "role=puppetmaster" > "$FACTER_PATH/facts.d/role.txt"
+
+# Puppet agent
+sudo puppet agent --enable
