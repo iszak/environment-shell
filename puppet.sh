d51f6e653914aeaf9c6f5927cfaa9fa844090f36
Iszak <iszak.bryan@gmail.com>
2015-11-11 22:02:07 +0000

    Always install latest gems

--- puppet.sh
+++ puppet.sh
@@ -15,8 +15,8 @@ sudo apt-get install puppet --yes
 apt-get install build-essential --yes
 
 # Puppet dependencies
-sudo gem install ruby-shadow --version=2.4.1
-sudo gem install deep_merge --version=1.0.1
+sudo gem install ruby-shadow
+sudo gem install deep_merge
 
 # Clean
 sudo rm -rf /etc/puppet/
